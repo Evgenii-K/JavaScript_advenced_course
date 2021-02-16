9d2dfeef69022804e108212679a2b9dac487280d
Evgenii-K <72011038+Evgenii-K@users.noreply.github.com>
2021-02-16 13:28:43 +0700

    lesson1; server v2.0

--- server.js
+++ server.js
@@ -1,26 +1,13 @@
 const http = require('http');
 const fs = require('fs');
 const port = process.env.PORT || 3000;
+const dirWay = './public/';
 
 http.createServer((req, res) => {
     // добавляем к запросу путь к папке
-    const filePath = './public/' + req.url.substr(1);
+    const filePath = dirWay + req.url.substr(1);
     console.log(`Запрошенный url: ${filePath}`);
-
-    if (filePath === './public/') {
-        res.end(fs.readFileSync('./public/index.html', 'utf8'));
-    } else {
-        fs.access(filePath, fs.constants.R_OK, err => {
-            // если произошла ошибка - отправляем статусный код 404
-            if(err){
-                res.statusCode = 404;
-                res.end("Resourse not found!");
-            }
-            else{
-                fs.createReadStream(filePath).pipe(res);
-            }
-        });
-    }
+    fs.createReadStream(filePath).pipe(res); 
 }).listen((port), () => {
     console.log(`Server started at ${port}`);
 });
