0987c1265565d545cea313f703b9c79913751759
Evgenii-K <72011038+Evgenii-K@users.noreply.github.com>
2021-02-16 13:34:57 +0700

    lesson1; server v1.1

--- server.js
+++ server.js
@@ -7,7 +7,21 @@ http.createServer((req, res) => {
     // добавляем к запросу путь к папке
     const filePath = dirWay + req.url.substr(1);
     console.log(`Запрошенный url: ${filePath}`);
-    fs.createReadStream(filePath).pipe(res); 
+
+    if (filePath === dirWay) {
+        res.end(fs.readFileSync('./public/index.html', 'utf8'));
+    } else {
+        fs.access(filePath, fs.constants.R_OK, err => {
+            // если произошла ошибка - отправляем статусный код 404
+            if(err){
+                res.statusCode = 404;
+                res.end("Resourse not found!");
+            }
+            else{
+                fs.createReadStream(filePath).pipe(res);
+            }
+        });
+    }
 }).listen((port), () => {
     console.log(`Server started at ${port}`);
 });
