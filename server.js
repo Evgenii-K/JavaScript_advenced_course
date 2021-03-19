835c3c0d4b42b453c8abbb439c68004185efd002
Evgenii-K <72011038+Evgenii-K@users.noreply.github.com>
2021-03-19 12:48:49 +0500

    Lesson_8; changed use express

--- server.js
+++ server.js
@@ -35,7 +35,7 @@ const bodyParser = require('body-parser');
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.use(express.static('./public'));
+app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.json());
 
 app.get('/database/:page', (req, res) => {
@@ -70,6 +70,9 @@ app.post('/cartlist', (req, res) => {
     });
 });
 
-app.listen(port, () => {
+app.listen(port, (err) => { 
+    if (err) {
+        console.log(`Oops: ${err}`);
+    }
     console.log(`Server started at ${port} port`);
 });
